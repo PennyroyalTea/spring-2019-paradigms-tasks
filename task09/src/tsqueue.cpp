851269c396923d27c8a221b32d25a4d66fbda9b3
Boris Starkov <boris.starkow@gmail.com>
2019-05-04 22:20:30 +0300

    fix signalling after pushing

--- task09/src/tsqueue.cpp
+++ task09/src/tsqueue.cpp
@@ -12,7 +12,10 @@ void threadsafe_queue_destroy(ThreadsafeQueue *q) {
 
 void threadsafe_queue_push(ThreadsafeQueue *q, void *data) {
     pthread_mutex_lock(&(q->m));
+
     queue_push(&(q->q), data);
+
+    pthread_cond_signal(&(q->cond));
     pthread_mutex_unlock(&(q->m));
 }
 
