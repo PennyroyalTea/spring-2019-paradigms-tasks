8a4dd5ef51a166672c170f4395f4c89ac7fe0d9d
Boris Starkov <boris.starkow@gmail.com>
2019-05-06 22:31:11 +0300

    rm unnecessary empty lines

--- task09/src/tsqueue.cpp
+++ task09/src/tsqueue.cpp
@@ -14,16 +14,13 @@ void threadsafe_queue_destroy(ThreadsafeQueue *q) {
 
 void threadsafe_queue_push(ThreadsafeQueue *q, void *data) {
     pthread_mutex_lock(&(q->m));
-
     queue_push(&(q->q), data);
-
     pthread_cond_signal(&(q->cond));
     pthread_mutex_unlock(&(q->m));
 }
 
 void *threadsafe_queue_wait_and_pop(ThreadsafeQueue *q) {
     pthread_mutex_lock(&(q->m));
-
     while (queue_empty(&(q->q))) {
         pthread_cond_wait(&(q->cond), &(q->m));
     }
