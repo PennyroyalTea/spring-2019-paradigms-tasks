6dce6855866a348836066f66821b24dab543ae42
Boris Starkov <boris.starkow@gmail.com>
2019-05-13 22:38:27 +0300

    do basics and test 'em

--- task10/src/Basics.hs
+++ task10/src/Basics.hs
@@ -6,39 +6,58 @@ module Basics where
 
 -- 1. head' возвращает первый элемент непустого списка
 head' :: [a] -> a
-head' = undefined
+head' (x:_) = x
 
 -- 2. tail' возвращает список без первого элемента, для пустого - пустой
 tail' :: [a] -> [a]
-tail' = undefined
+tail' (_:xs) = xs
+tail' _      = []
 
 -- 3. take' возвращает первые n >= 0 элементов исходного списка
 take' :: Int -> [a] -> [a]
-take' = undefined
+take' 0 _      = []
+take' n (x:xs) = x : take' (n - 1) xs
+take' _ _      = []
 
 -- 4. drop' возвращает список без первых n >= 0 элементов; если n больше длины
 -- списка, то пустой список.
 drop' :: Int -> [a] -> [a]
-drop' = undefined
+drop' 0 xs = xs
+drop' n (_:xs) = drop' (n-1) xs 
+drop' _ _ = []
 
 -- 5. filter' возвращает список из элементов, для которых f возвращает True
 filter' :: (a -> Bool) -> [a] -> [a]
-filter' f xs = undefined
+filter' f (x:xs) | f x       = x : filter' f xs
+                 | otherwise = filter' f xs
+filter' _ _                  = []
 
 -- 6. foldl'' последовательно применяет функцию f к элементу списка l и значению,
 -- полученному на предыдущем шаге, начальное значение
 -- foldl'' (+) 0 [1, 2, 3] == (((0 + 1) + 2) + 3)
 -- foldl'' (*) 4 [] == 4
 foldl'' :: (a -> b -> a) -> a -> [b] -> a
-foldl'' f z l = undefined
+foldl'' f z (x:xs) = foldl'' f (f z x) xs
+foldl'' _ z _      = z
 
 -- 7. concat' принимает на вход два списка и возвращает их конкатенацию
 -- concat' [1,2] [3] == [1,2,3]
 concat' :: [a] -> [a] -> [a]
-concat' = undefined
+concat' (x:xs) ys = x : concat' xs ys
+concat' _      ys = ys
 
 -- 8. quickSort' возвращает его отсортированный список
 -- quickSort' должен быть реализован через алгоритм QuickSort
 -- (выбор pivot может быть любым)
 quickSort' :: Ord a => [a] -> [a]
-quickSort' = undefined
+quickSort' [] = []
+quickSort' xs = 
+    let 
+    pivot = xs !! (length xs `div` 2) 
+    left = filter' (<pivot) xs
+    mid = filter' (==pivot) xs
+    right = filter' (>pivot) xs
+    in concat' (quickSort' left) $ concat' mid (quickSort' right)
+
+
+
--- task10/test/TestBasics.hs
+++ task10/test/TestBasics.hs
@@ -10,24 +10,38 @@ testsBasics :: TestTree
 testsBasics = testGroup "Unit tests for Basics tasks"
     [testCase "head' works on non-empty list" $
         head' [1,2,3] @?= 1
+    , testCase "head' works on an infinite list" $
+        head' [1,2..] @?= 1
 
     , testCase "tail' works on non-empty list too" $
         tail' [1,2,3] @?= [2,3]
+    , testCase "tail' works on infinite list too" $
+        (tail' [1,2..]) !! 100 @?= 102
 
     , testCase "take' takes 1 element from 3-element list" $
         take' 1 [1,2,3] @?= [1]
+    , testCase "take' takes 5 elems from infinite list" $
+        take' 5 [1..] @?= [1, 2, 3, 4, 5]
 
     , testCase "drop' drops 1 element from 3-element list" $
         drop' 1 [1,2,3] @?= [2,3]
+    , testCase "drop' drops 100 elements from infinite list" $
+        head' (drop' 100 [1..]) @?= 101
 
     , testCase "filter' selects only even numbers from 0 to 10" $
         filter' even [0..10] @?= [0,2..10]
+    , testCase "filter' selects only even numbers from infinite list" $
+        filter' even [1..] !! 4 @?= 10
 
     , testCase "foldl'' can be used for finding sum of elements" $
         foldl'' (+) 0 [1,2,3] @?= 6
 
     , testCase "concat' works on finite lists as expected" $
         concat' [1,2,3] [4,5,6] @?= [1..6]
+    , testCase "concat' works on finite + infinite" $
+        (concat' [-1, -2, -3] [1..]) !! 102 @?= 100
+    , testCase "concat' works on infinite + infinite" $
+        (concat' [1..] [1..]) !! 1000 @?= 1001
 
     , testCase "quickSort actualy sorts the list" $
         quickSort' [5,2,3,4,1] @?= [1..5]
