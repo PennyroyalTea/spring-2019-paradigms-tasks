63d36bb673a5ddc5bc71c92f580bb63f295be9c7
Boris Starkov <boris.starkow@gmail.com>
2019-05-13 23:21:41 +0300

    fix codestyle Robots.hs

--- task10/src/Robots.hs
+++ task10/src/Robots.hs
@@ -77,8 +77,8 @@ isAlive (_, _, health) = health > 0
 -- вернуть второго робота, как будто ничего и не было
 fight :: Robot -> Robot -> Robot
 fight attacker defender 
-                | not (isAlive attacker) = defender
-                | otherwise              = setHealth (getHealth defender - getAttack attacker) $ defender
+    | isAlive attacker = damage defender $ getAttack attacker
+    | otherwise        = defender
 
 -- Наконец, напишите функцию, которая бы моделировала три раунда схватки между
 -- двумя роботами и возвращала бы победителя. Схватка происходит следующим образом:
@@ -89,10 +89,10 @@ fight attacker defender
 -- Победитель определеяется как робот, у которого уровень здоровья строго больше, чем у сопереника
 -- Если же так вышло, что после трех раундов у обоих роботов одинаковый уровень жизни, то
 -- победителем считается тот, кто ударял первым(то есть атакующий робот)
-fightIteration::Bool->Int->Robot->Robot->Robot
+fightIteration :: Bool -> Int -> Robot -> Robot -> Robot
 fightIteration _ 0 attacker defender
-                | getHealth defender > getHealth attacker = defender
-                | otherwise                               = attacker
+    | getHealth defender > getHealth attacker = defender
+    | otherwise                               = attacker
 
 fightIteration True n attacker defender = fightIteration False (n - 1) attacker $ fight attacker defender
 fightIteration False n attacker defender = fightIteration True (n - 1) (fight defender attacker) defender
