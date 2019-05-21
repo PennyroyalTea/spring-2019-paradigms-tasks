35e4c60f0dd35fa81225938c3c09ff030feab19d
Boris Starkov <boris.starkow@gmail.com>
2019-05-22 01:35:25 +0300

    fix style 05

--- task12/05.sql
+++ task12/05.sql
@@ -1,7 +1,7 @@
 -- Выведите форму правления с максимальной суммарной площадью стран, которые её
 -- придерживаются (вывод: форма правления и суммарная площадь). (0,25 баллов)
-SELECT GovernmentForm, SUM(SurfaceArea)
+SELECT GovernmentForm, SUM(SurfaceArea) AS S
 FROM Country
 GROUP BY GovernmentForm
-ORDER BY SUM(SurfaceArea) DESC
+ORDER BY S DESC
 LIMIT 1;
