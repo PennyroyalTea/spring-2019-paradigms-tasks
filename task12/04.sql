4d098a9a6963ab67b23711fb871506d1c45b47b8
Boris Starkov <boris.starkow@gmail.com>
2019-05-22 12:48:48 +0300

    fix sorting

--- task12/04.sql
+++ task12/04.sql
@@ -4,7 +4,8 @@
 -- данных могут быть страны без городов вообще (например, информации о городах
 -- нет, или кто-то посчитал Антарктиду страной), для таких стран нужно
 -- вывести 0 (0,75 баллов).
-SELECT Country.Name, SUM(CASE WHEN City.Population >= 1000000 THEN 1 ELSE 0 END)
+SELECT Country.Name, SUM(CASE WHEN City.Population >= 1000000 THEN 1 ELSE 0 END) AS S
 FROM Country 
 LEFT JOIN City ON Country.Code = City.CountryCode
-GROUP BY Country.Code;
+GROUP BY Country.Code
+ORDER BY S;
